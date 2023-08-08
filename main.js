99c7fd71161a7158773339b233b1e4951098d026
Natália Oliveira <63076575+natoliveira@users.noreply.github.com>
2023-08-08 16:17:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const carro = {marca: "fiat", 
+modelo: "uno",
+ano: "1989",
+cor: "prata",
+placa: "rpq895",
+portas:"quatro",
+motor: function(){alert('galooooooooooooooo')}
+};
+console.log (carro.motor())
